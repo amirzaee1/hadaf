a4ce2135c6ad0ec4fa3a01a15d5b90feb2365bf6
amirzaee1 <amirzaee123456789@gmail.com>
2026-09-06 21:35:59 +0330

    نمایش واقعی فانوس و ساده‌سازی عنوان صفحه

--- src/components/CinematicCanvas.tsx
+++ src/components/CinematicCanvas.tsx
@@ -13,18 +13,13 @@ export const CinematicCanvas: React.FC<CinematicCanvasProps> = ({ activeChapter 
     />
     <div className="absolute inset-0 bg-[radial-gradient(circle_at_50%_35%,rgba(34,50,84,.22),transparent_58%)]" />
     {activeChapter === 4 && (
-      <svg viewBox="0 0 640 420" className="absolute bottom-0 left-1/2 hidden h-[62vh] w-[1100px] -translate-x-1/2 opacity-55 md:block" aria-hidden="true">
+      <svg viewBox="0 0 640 420" className="absolute bottom-0 hidden h-[64vh] w-[760px] -translate-x-1/2 opacity-70 md:block" style={{ left: 'calc(50% - 350px)' }} aria-hidden="true">
         <defs>
-          <linearGradient id="lighthouse-beam-left" x1="1" y1="0" x2="0" y2="0">
-            <stop offset="0" stopColor="#fde68a" stopOpacity=".52" />
-            <stop offset="1" stopColor="#fde68a" stopOpacity="0" />
-          </linearGradient>
           <linearGradient id="lighthouse-beam-right" x1="0" y1="0" x2="1" y2="0">
             <stop offset="0" stopColor="#fde68a" stopOpacity=".52" />
             <stop offset="1" stopColor="#fde68a" stopOpacity="0" />
           </linearGradient>
         </defs>
-        <path d="M310 105 L28 20 L28 190 Z" fill="url(#lighthouse-beam-left)" />
         <path d="M330 105 L612 28 L612 182 Z" fill="url(#lighthouse-beam-right)" />
         <path d="M285 362 L305 118 L335 118 L355 362 Z" fill="#e7e5e4" opacity=".72" />
         <path d="M292 282 L348 282 M297 222 L343 222 M302 162 L338 162" stroke="#c2413a" strokeWidth="18" opacity=".75" />
